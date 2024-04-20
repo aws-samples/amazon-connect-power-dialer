e61af7cdda72db19d7d12213fd92d9896bcefaae
Victor Palomo <vicpalg@amazon.com>
2024-04-19 23:55:45 -0600

    Fixed parameter retrieval

--- PowerDialer-getConfig/lambda_function.py
+++ PowerDialer-getConfig/lambda_function.py
@@ -3,23 +3,34 @@ import json
 import boto3
 import os
 
+ssm=boto3.client('ssm')
+
 def lambda_handler(event, context):
     DIALER_DEPLOYMENT = os.environ['DIALER_DEPLOYMENT']
     config=get_parameters(DIALER_DEPLOYMENT)
     return config
     
 def get_parameters(deployment):
-    ssm=boto3.client('ssm')
-    try:
-        ssmresponse = ssm.get_parameters_by_path(Path='/connect/dialer/'+deployment+'/',Recursive=True)
+    
+    config={}
+    next_token = None
 
+    try:
+        while True:
+            if next_token:
+                ssmresponse = ssm.get_parameters_by_path(Path='/connect/dialer/'+deployment+'/',NextToken=next_token)
+            else:
+                ssmresponse = ssm.get_parameters_by_path(Path='/connect/dialer/'+deployment+'/')
+            for parameter in ssmresponse['Parameters']:
+                config[parameter['Name'].split("/")[-1]]=parameter['Value']
+            
+            next_token = ssmresponse.get('NextToken')
+
+            if not next_token:
+                break
+            
     except:
+        print("Error getting config")
         return None
     else:
-        config={}
-        for parameter in ssmresponse['Parameters']:
-            config[parameter['Name'].split("/")[-1]]=parameter['Value']
         return config
-
-
-    
